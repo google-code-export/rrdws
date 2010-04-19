da990a34b6abfcc7dcb143948f8ec422b1f0e367
gennady <gennady@localhost>
2010-04-19 22:40:51 +0200

    check  with http://mail-archives.apache.org/mod_mbox//xmlgraphics-batik-users/200504.mbox/%3C001101c53933$2fb47c80$0b01010a@DEV5%3E

--- ws.rrd.pdf/SVG2PDF.java
+++ ws.rrd.pdf/SVG2PDF.java
@@ -4,6 +4,7 @@ import java.awt.print.PageFormat;
 import java.awt.print.Paper;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.StringReader;
 
 import org.apache.batik.transcoder.TranscoderInput;
 import org.apache.batik.transcoder.print.PrintTranscoder;
@@ -11,6 +12,7 @@ import org.apache.batik.transcoder.print.PrintTranscoder;
 import com.lowagie.text.Document;
 import com.lowagie.text.DocumentException;
 import com.lowagie.text.ImgTemplate;
+import com.lowagie.text.PageSize;
 import com.lowagie.text.Paragraph;
 import com.lowagie.text.pdf.PdfContentByte;
 import com.lowagie.text.pdf.PdfTemplate;
@@ -26,47 +28,48 @@ import com.lowagie.text.pdf.PdfWriter;
  * Creation:  Apr 19, 2010::9:53:46 PM<br> 
  */
 public class SVG2PDF {
-	public static void main(String[] args) {
-
-        Document document = new Document();
+	   /** Creates a new instance of Main */
+    final String svg = "<?xml version='1.0' encoding='ISO-8859-1' standalone='no'?>" +
+            "<svg width='1188' height='756' xmlns='http://www.w3.org/2000/svg'>" +
+            " <g transform='translate(50,50) scale(5,5)'>" +
+            "  <rect x='0.0' y='0' width='200.0' height='100.0' style='fill:red; stroke:black; stroke-width:.2; fill-opacity: 0.2;' />" +
+            "  <rect x='20.0' y='20' width='100.0' height='50.0' style='fill:red; stroke:black; stroke-width:.2; fill-opacity: 0.4;' />" +
+            "  <rect x='40.0' y='40' width='50.0' height='10.0' style='fill:blue; stroke:black; stroke-width:.2; fill-opacity: 0.1;' />" +
+            "  <text x='45.0' y='45' >HelloAbcde</text>" +
+            " </g>" +
+            "</svg>";
+    
+    public SVG2PDF() {
+        com.lowagie.text.Document document = new
+com.lowagie.text.Document(PageSize._11X17.rotate());
         try {
-            PdfWriter writer = PdfWriter.getInstance(document,
-                    new FileOutputStream("svg.pdf"));
+            PdfWriter writer = PdfWriter.getInstance(document,	new
+FileOutputStream("svgsize.pdf"));
             document.open();
-            document.add(new Paragraph("SVG Example"));
-
-            int width = 250;
-            int height = 250;
             PdfContentByte cb = writer.getDirectContent();
-            PdfTemplate template = cb.createTemplate(width,height);
-            BufferedImage image = new BufferedImage(width,height, BufferedImage.TYPE_INT_RGB); 
-            Graphics2D g2 = //template.createGraphics(width,height);
-            	//new LocalG2D();
-            	image .createGraphics();
-            
+            cb.saveState();
+            cb.concatCTM(1.0f, 0, 0, 1.0f , 36, 0 );
+            Graphics2D g2 = cb.createGraphics(1188, 756);
             PrintTranscoder prm = new PrintTranscoder();
-            TranscoderInput ti = //new TranscoderInput("file:///c:\\java\\svg.xml");
-            	new TranscoderInput(PageFormat.class.getClassLoader().getSystemResourceAsStream("test.svg"));
+            TranscoderInput ti = new TranscoderInput( new StringReader(svg));
             prm.transcode(ti, null);
-           
             PageFormat pg = new PageFormat();
             Paper pp= new Paper();
-            pp.setSize(width, height);
-            pp.setImageableArea(0, 0, width, height);
+            pp.setSize( 1188, 756);
+            pp.setImageableArea(0, 0, 1188, 756);
             pg.setPaper(pp);
-            prm.print(g2, pg, 0); 
-            g2.dispose(); 
-
-            ImgTemplate img = new ImgTemplate(template);           
-            document.add(img); 
-        } catch (DocumentException e) {
-            System.err.println(e);
-        } catch (IOException e) {
-            System.err.println(e);
+            prm.print(g2, pg, 0);
+            g2.dispose();
+            cb.restoreState();
+            document.close();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        document.close();
+     }
+    public static void main(String[] args) {
+    	SVG2PDF m = new SVG2PDF();
+    }
 
-      }
 
 }
 
