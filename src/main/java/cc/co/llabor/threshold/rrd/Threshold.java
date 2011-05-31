cb7faa838d9adafe81074f536ae1429e934e9dde
vasiliij pupkin <vasiliij.pupkin@googlemail.com>
2011-05-31 19:17:40 +0200

    TC7.0.x need the dep  for JSPs  :(removed for TC.6.0)::: <dependency> <groupId>javax.servlet.jsp</groupId><artifactId>jsp-api</artifactId>    <version>2.1</version></dependency>

---
+++ src/main/java/cc/co/llabor/threshold/rrd/Threshold.java
@@ -0,0 +1,16 @@
+package cc.co.llabor.threshold.rrd;
+/** 
+ * <b>Description:TODO</b>
+ * @author      vipup<br>
+ * <br>
+ * <b>Copyright:</b>     Copyright (c) 2006-2008 Monster AG <br>
+ * <b>Company:</b>       Monster AG  <br>
+ * 
+ * Creation:  23.05.2011::14:06:42<br> 
+ */
+public interface Threshold {
+
+}
+
+
+ 
