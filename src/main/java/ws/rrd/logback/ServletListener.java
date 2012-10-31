b6c05990e61a0efdfeb5625b4e0d8edc57537e27
vasiliij pupkin <vasiliij.pupkin@googlemail.com>
2012-10-31 16:02:59 +0100

    listenet 4 jmxAppender ++

---
+++ src/main/java/ws/rrd/logback/ServletListener.java
@@ -0,0 +1,47 @@
+package ws.rrd.logback;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import ws.rrd.xpath.XPathContentHandler;
+
+/** 
+ * <b>Description:TODO</b>
+ * @author      vipup<br>
+ * <br>
+ * <b>Copyright:</b>     Copyright (c) 2006-2008 Monster AG <br>
+ * <b>Company:</b>       Monster AG  <br>
+ * 
+ * Creation:  31.10.2012::15:50:22<br> 
+ */
+public class ServletListener implements ServletContextListener{
+	
+	private static final Logger log = LoggerFactory.getLogger(ServletListener.class  );
+
+	@Override
+	public void contextInitialized(ServletContextEvent sce) {
+		log.info("!!!!!!!!!!!!!!!!!!rrd contextInitialized!!!!!!!!!!!!!");
+	}
+
+	@Override
+	public void contextDestroyed(ServletContextEvent sce) {
+		log.info("..............rrd contextDestroyed...................");
+		if (appenderToKill!=null){
+			appenderToKill.close();
+			appenderToKill = null;
+		}
+		log.info(".....................................................");
+	}
+
+	static JMXAppender appenderToKill =null;
+	public static void registerToKilling(JMXAppender jmxAppender) {
+		appenderToKill = jmxAppender;
+	}
+
+}
+
+
+ 
